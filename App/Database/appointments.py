ab08f7687ece5f7f322218eaee4b096dee8a8d05
Matteo_Gan <mgandini02@gmail.com>
2025-05-28 19:25:30 +0200

    expansion appointment

--- App/Database/appointments.py
+++ App/Database/appointments.py
@@ -7,20 +7,28 @@ c.execute("""DELETE FROM Appointments;""")
 
 # Appointments with (slot_tempo, doctor_id, patient_id, visit_type)
 appointments = [
-    ('2025-04-15 12:30', 1, 8, 1), 
-    ('2025-04-30 16:30', 1, 8, 2), 
-    ('2025-05-29 08:30', 1, 5, 1),
-    ('2025-05-29 16:30', 1, 7, 2),
-    ('2025-06-15 10:00', 1, 5, 2),
-    ('2025-05-30 09:00', 1, 8, 2),
-    ('2025-05-30 17:00', 1, 6, 1),
+    ('2025-04-15 12:30', 1, 8, 1, 0), 
+    ('2025-04-30 16:30', 1, 8, 2, 0), 
+    ('2025-05-29 08:30', 1, 5, 1, 0),
+    ('2025-05-29 16:30', 1, 7, 2, 0),
+    ('2025-06-15 10:00', 1, 5, 2, 0),
+    ('2025-05-30 09:00', 1, 8, 2, 0),
+    ('2025-05-30 17:00', 1, 6, 1, 0),
+    ('2025-04-15 12:30', 2, None, None, 1),
+    ('2025-04-30 16:30', 2, None, None, 1),
+    ('2025-05-29 08:30', 2, None, None, 1),
+    ('2025-05-29 16:30', 2, None, None, 1),
+    ('2025-06-15 10:00', 2, None, None, 1),
+    ('2025-05-30 09:00', 2, None, None, 1),
+    ('2025-06-30 09:00', 1, None, None, 1),
+    ('2025-06-30 17:00', 1, None, None, 1),
 ]
 
-for slot_tempo, doctor_id, patient_id, visit_type in appointments:
+for slot_tempo, doctor_id, patient_id, visit_type, disp in appointments:
     c.execute("""
-        INSERT INTO Appointments(slot_tempo, doctor, patient, visit_type)
-        VALUES (?, ?, ?, ?)
-    """, (slot_tempo, doctor_id, patient_id, visit_type))
+        INSERT INTO Appointments(slot_tempo, doctor, patient, visit_type, dispo)
+        VALUES (?, ?, ?, ?, ?)
+    """, (slot_tempo, doctor_id, patient_id, visit_type, disp))
 
 connection.commit()
 connection.close()
